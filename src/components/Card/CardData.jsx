de7353007deca0604e39c68afaae783b61d96b22
Kishore Mongar <84139548+fushigina-Shinobi@users.noreply.github.com>
2022-07-23 10:53:17 +0530

    added card data

--- src/components/Card/CardData.jsx
+++ src/components/Card/CardData.jsx
@@ -2,7 +2,7 @@ const CardData = [
   {
     id: 1,
     imageUrl:
-      "https://res.cloudinary.com/fushigina-shinobi/image/upload/v1655285002/luis-villasmil-mlVbMbxfWI4-unsplash5_sr9kid.jpg",
+      "https://res.cloudinary.com/fushigina-shinobi/image/upload/v1658510322/task_g8sgw8.jpg",
     title: "Task To Settle",
     info: "Write the task you need to settle. Made using React js and web localstorage.",
     visitLink: "https://task-to-settle.netlify.app/",
@@ -11,7 +11,7 @@ const CardData = [
   {
     id: 2,
     imageUrl:
-      "https://res.cloudinary.com/fushigina-shinobi/image/upload/v1653818572/Trail%20Addek%20Slides/PSX_20191225_171616_mvwgd3.jpg",
+      "https://res.cloudinary.com/fushigina-shinobi/image/upload/v1658510322/cocktail_r5fvds.jpg",
     title: "Cocktail Catalog",
     info: "Catalog of various cockatails, find the ingridents and take note of how to make it.",
     visitLink: "https://thecocktailcatalog.netlify.app/",
@@ -20,7 +20,7 @@ const CardData = [
   {
     id: 3,
     imageUrl:
-      "https://res.cloudinary.com/fushigina-shinobi/image/upload/v1655282482/ian-dooley-DJ7bWa-Gwks-unsplash_skheix.jpg",
+      "https://res.cloudinary.com/fushigina-shinobi/image/upload/v1658506543/laptop_jmrtge.jpg",
     title: "Portfolio",
     info: "Simple React Js portfolio with the help of FC, props & hooks",
     visitLink: "https://mongarkishore.netlify.app/",
@@ -29,20 +29,20 @@ const CardData = [
   {
     id: 4,
     imageUrl:
-      "https://res.cloudinary.com/fushigina-shinobi/image/upload/v1653818594/Trail%20Addek%20Slides/PicsArt_05-06-03.35.11_coa0vj.jpg",
+      "https://res.cloudinary.com/fushigina-shinobi/image/upload/v1654791402/Trail%20Addek%20Slides/PSX_20191225_205212_ssxzdz.jpg",
     title: "Trail Addek",
     info: "Are you looking for offbeat destination ? We provide you with the best offbeat trek destination.",
-    visitLink: "",
+    visitLink: "https://trailaddek.netlify.app",
     sourceLink: "https://github.com/fushigina-Shinobi/trail-addek",
   },
 
   {
     id: 5,
     imageUrl: "https://picsum.photos/401/300",
-    title: "Popcorn Movie",
-    info: "Coming Soon!!! Lorem ipsum dolor sit amet consectetur adipisicing elit Ea alias et",
+    title: "Anime Freak",
+    info: "Search information regarding your favourite anime and it's various aspects of data that you're curious about",
     visitLink: "https://monga-kishore.netlify.app/",
-    sourceLink: "https://task-to-settle.netlify.app/",
+    sourceLink: "https://github.com/fushigina-Shinobi/anime-freak",
   },
   {
     id: 6,
@@ -66,8 +66,17 @@ const CardData = [
     title: "Quiz App",
     info: "Coming Soon!!! Lorem ipsum dolor sit amet consectetur adipisicing elit Ea alias et",
     visitLink: "https://monga-kishore.netlify.app/",
-    sourceLink: "https://task-to-settle.netlify.app/",
+    sourceLink: "https://github.com/fushigina-Shinobi/quizz-app",
   },
 ];
 
 export default CardData;
+
+/* {
+    id: 5,
+    imageUrl: "https://picsum.photos/401/300",
+    title: "Popcorn Movie",
+    info: "Coming Soon!!! Lorem ipsum dolor sit amet consectetur adipisicing elit Ea alias et",
+    visitLink: "https://monga-kishore.netlify.app/",
+    sourceLink: "https://task-to-settle.netlify.app/",
+  }, */
